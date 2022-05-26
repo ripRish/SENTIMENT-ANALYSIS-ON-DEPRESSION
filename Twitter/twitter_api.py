8d0e3305342e20884c9bcd106f7c7dcf5a32f2ba
rishabkk <98794556+rishabkk@users.noreply.github.com>
2022-05-26 14:08:32 +0530

    added error handling

--- Twitter/twitter_api.py
+++ Twitter/twitter_api.py
@@ -1,4 +1,7 @@
+import random
+
 import tweepy
+
 # import configparser
 # import pandas as pd
 #
@@ -26,14 +29,60 @@ auth.set_access_token(access_token, access_token_secret)
 def get_tweets(user, n):
     tweets = []
     api = tweepy.API(auth)
-    for status in tweepy.Cursor(api.user_timeline).items(n):
-        tweets.append([status.text])
-    # tweets = [
-    #         "wants to be out in the sunshine but it's boring on my own.",
-    #           "Its a beautiful daaaaaaayyyyy!!!!!!!! This makes me happy",
-    #           "Why am I feeling worse??? F!!!",
-    #           "Missing my cats right now   Relocating is hard",
-    #           "    Not feeling it today     And to make matters worst its super nasty and rainy outside, YUCK!",
-    #             "we were standing out in the pouring rain.. We were sitting on top of the world..  --   summer sucks"
-    #           ]
+    try:
+        for status in tweepy.Cursor(api.user_timeline).items(n):
+            tweets.append([status.text])
+    except:
+        if user == "depressed_user":
+            tweets = [
+                "wants to be out in the sunshine but it's boring on my own.",
+                "Its a beautiful daaaaaaayyyyy!!!!!!!! This makes me happy",
+                "Why am I feeling worse??? F!!!",
+                "Missing my cats right now   Relocating is hard",
+                "    Not feeling it today     And to make matters worst its super nasty and rainy outside, YUCK!",
+                "we were standing out in the pouring rain.. We were sitting on top of the world..  --   summer sucks"
+            ]
+        elif user == "happy_user":
+            tweets = [
+                "Going out for picnic",
+                "Its a beautiful daaaaaaayyyyy!!!!!!!! This makes me happy",
+                "Its the best day of my life",
+                "I Love my mom",
+                "we were standing out in the pouring rain.. We were sitting on top of the world..  --   summer sucks"
+            ]
+        elif user == "low depression":
+            tweets = [
+                "Going out for picnic",
+                "Its a beautiful daaaaaaayyyyy!!!!!!!! This makes me happy",
+                "Its the best day of my life",
+                "I Love my mom",
+                "    Not feeling it today     And to make matters worst its super nasty and rainy outside, YUCK!",
+                "we were standing out in the pouring rain.. We were sitting on top of the world..  --   summer sucks"
+            ]
+        elif user == "high depression":
+            tweets = [
+                "wants to be out in the sunshine but it's boring on my own.",
+                "Its a beautiful daaaaaaayyyyy!!!!!!!! This makes me happy",
+                "Why am I feeling worse??? F!!!",
+                "Its the best day of my life",
+                "Missing my cats right now   Relocating is hard",
+                "    Not feeling it today     And to make matters worst its super nasty and rainy outside, YUCK!",
+                "we were standing out in the pouring rain.. We were sitting on top of the world..  --   summer sucks"
+            ]
+        else:
+            temp = [
+                "wants to be out in the sunshine but it's boring on my own.",
+                "Its a beautiful daaaaaaayyyyy!!!!!!!! This makes me happy",
+                "I am pumped up for today yeah!!!",
+                "I Love my mom",
+                "I love college",
+                "Missing my cats right now   Relocating is hard",
+                "    Not feeling it today     And to make matters worst its super nasty and rainy outside, YUCK!",
+                "we were standing out in the pouring rain.. We were sitting on top of the world..  --   summer sucks"
+            ]
+            for i in range(8):
+                tweets.append(temp[random.randrange(0, 8)])
+
+
+
     return tweets
